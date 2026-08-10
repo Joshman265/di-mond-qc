e5a7d2b49f5df18f2f2cf50fb24fd16aa675213e
Joshman265 <joshdilillo2@gmail.com>
2026-08-10 12:28:22 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,42 @@
-const CACHE='dimond-qc-v3'; const ASSETS=['./','./index.html','./app.js','./manifest.webmanifest','./icon.svg']; self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))); self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE = 'dimond-qc-v4';
+const ASSETS = [
+  './',
+  './index.html',
+  './app.js',
+  './manifest.webmanifest',
+  './icon.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys =>
+        Promise.all(
+          keys
+            .filter(key => key !== CACHE)
+            .map(key => caches.delete(key))
+        )
+      )
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
